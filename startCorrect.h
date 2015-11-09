98f696882c7511240cd1848c69661a0257417487
Kyle Al-Shafei <kalshafei@gmail.com>
2015-11-09 01:09:06 -0500

    Start Screen files

---
+++ startCorrect.h
@@ -0,0 +1,6 @@
+#ifndef STARTCORRECT_BITMAP_H
+#define STARTCORRECT_BITMAP_H
+extern const unsigned short startCorrect[38400];
+#define STARTCORRECT_WIDTH 240
+#define STARTCORRECT_HEIGHT 160
+#endif
